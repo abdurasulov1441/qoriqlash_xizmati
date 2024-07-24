4b78cdefd277618e2115dcbb793216af3439305a
Abdulaziz Abdurasulov <abdurasulov1024@gmail.com>
2024-07-24 19:55:44 +0500

    abdulaziz

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:qoriqlash_xizmati/model/users_model.dart';
+
 import 'package:qoriqlash_xizmati/qoriqlash_xizmati_app.dart';
 
 Future<void> main() async {
