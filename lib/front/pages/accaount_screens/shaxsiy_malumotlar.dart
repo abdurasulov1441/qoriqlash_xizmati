6128e403bd31f021c1cd0acfc5a219729ae346f3
unknown <abdurasulov1024@gmail.com>
2024-07-27 20:07:01 +0500

    hacker

--- lib/front/pages/accaount_screens/shaxsiy_malumotlar.dart
+++ lib/front/pages/accaount_screens/shaxsiy_malumotlar.dart
@@ -9,7 +9,8 @@ class ShaxsiyMalumotlar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      backgroundColor: AppColors.lightHeaderColor,
+      body: SingleChildScrollView(
         child: Column(
           children: [
             MiniRedAppBar(),
@@ -30,8 +31,48 @@ class ShaxsiyMalumotlar extends StatelessWidget {
                 children: [
                   Row(
                     children: [
+                      SizedBox(
+                        width: 20,
+                      ),
                       Text(
                         'F.I.SH',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.text,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: 'Rahimov Voris Avazbek o\'g\'li',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.person,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Telefon raqam',
                         style: AppStyle.fontStyle
                             .copyWith(fontWeight: FontWeight.bold),
                       )
@@ -52,15 +93,203 @@ class ShaxsiyMalumotlar extends StatelessWidget {
                         border: OutlineInputBorder(
                             borderRadius:
                                 BorderRadius.all(Radius.circular(15))),
-                        hintText: 'Rahimov Voris Avazbek o\'g\'li',
+                        hintText: '+998 (99) 786-25-51',
                         hintStyle: AppStyle.fontStyle,
                         label: Icon(
-                          Icons.person,
+                          Icons.phone,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Passport seriyasi va raqami',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.text,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: 'AA 1234567',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.edit_document,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Passport berilgan sana',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.phone,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: '15.02.2018',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.date_range,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Passport kim tomonidan berilgan',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.phone,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: 'Toshkent viloyati Qibray tumani',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.corporate_fare,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'JSHSHIR',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.phone,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: '12 34 56 78 90 12 34',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.code,
+                          color: AppColors.lightIconGuardColor,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Manzil',
+                        style: AppStyle.fontStyle
+                            .copyWith(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: AppColors.lightTextColor),
+                    keyboardType: TextInputType.phone,
+                    autocorrect: false,
+                    // controller: _phoneController,
+
+                    decoration: const InputDecoration(
+                        fillColor: AppColors.fillColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        hintText: 'Toshkent vil. M.Ulug’bek tum. A.Temur MFY',
+                        hintStyle: AppStyle.fontStyle,
+                        label: Icon(
+                          Icons.location_on,
                           color: AppColors.lightIconGuardColor,
                         )),
                   ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: 80,
             )
           ],
         ),
